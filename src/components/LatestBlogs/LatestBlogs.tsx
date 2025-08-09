595905229e5768e206a8ca1fb2f6474194374d40
Sawom <asawom250@gmail.com>
2025-08-09 17:18:57 +0600

    blogs pages working

---
+++ src/components/LatestBlogs/LatestBlogs.tsx
@@ -0,0 +1,18 @@
+const LatestBlogs = () => {
+  return (
+    <div>
+      <h2 className="text-3xl text-center my-5">
+        Latest Blogs from <span className="text-accent">Blogiz</span>
+      </h2>
+      <p className="text-xl w-2/4 text-gray-400 mx-auto text-center ">
+        <i>
+          Quantum computing is an advanced field of computing that leverages the
+          principles of quantum mechanics to process information in
+          fundamentally different ways from classical computers.
+        </i>
+      </p>
+    </div>
+  );
+};
+
+export default LatestBlogs;
