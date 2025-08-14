d4a7561c34ad6a2fa5c861012d50a1ce18dc5175
Sawom <asawom250@gmail.com>
2025-08-14 21:01:55 +0600

    provider setup

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-const Providers = ({children}: {children: React.ReactNode}) => {
-    return (
-        <div>
-            
-        </div>
-    );
+import { store } from "@/redux/api/baseApi";
+import { Provider } from "react-redux";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return <Provider store={store}>{children}</Provider>;
 };
 
 export default Providers;
+
+// here store={store} I created. { children: React.ReactNode } I get from layout.tsx
+// Provider from redux
